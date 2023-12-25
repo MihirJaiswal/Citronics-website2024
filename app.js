a412cebaa9439d79c088106fae7596ff998b3c59
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2023-12-25 12:05:11 +0530

    some changes

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.navbar');
+let navbar = document.querySelector('.header .navbar');
 
-function toggleButton() {
-    navbar.classList.toggle('show');  
+menu.onclick = () => { 
+    menu.classList.toggle('fas-times');
+    navbar.classList.toggle('active');
 }
 
-menu.addEventListener('click', toggleButton)
+window.onscroll = () => {
+    menu.classList.remove('fa-times'); 
+    navbar.classList.remove('active'); 
+}
+
+const observers = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visi');
+        } else {
+            entry.target.classList.remove('visi');
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.notv');
+hiddenElements.forEach((el) => {
+    observers.observe(el);
+})
