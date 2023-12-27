b17721836e5ed377d30ed1f092594ac4db28f489
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2023-12-27 07:24:27 +0530

    some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [".{html,js}"],
+  content: ["**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
