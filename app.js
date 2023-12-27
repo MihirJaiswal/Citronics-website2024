9eb81cdb7b33f5041c6c0c42f88ce9bfb8731d23
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2023-12-27 08:08:05 +0530

    some changes

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+var countDownDate = new Date("feb 25, 2024 00:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.querySelector("#days").innerHTML = days;
+
+  if(distance < 0) {
+    clearInterval(x);
+    document.querySelector("#days").innerHTML = 0;
+    document.querySelector("#hours").innerHTML = 0;
+    document.querySelector("#minutes").innerHTML = 0;
+    document.querySelector("#seconds").innerHTML = 0;
+  }
+
+}, 1000);
+
+
+
 
 window.onload = function() {
   gsap.from("#myVideo", {
